d8985dae142e1215d10592d12fc8ddebadf62323
waday1 <g1527079@st.t-kougei.ac.jp>
2016-12-19 16:30:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+var basicAuth = require('basic-auth-connect');
+app.use(basicAuth('hoge', 'hogepass'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
